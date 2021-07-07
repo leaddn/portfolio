5bd70a734f322684a37c19768d31e51946a8e28b
leaddn <leadieudonat@gmail.com>
2021-07-07 13:30:11 +0200

    portfolio v2

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+ 
+    $("#toggle-mode").on("click", function(event){
+        /*
+       $("body").toggleClass("light-mode");
+       $("body").toggleClass("dark-mode");
+       $(".black-bg").css("background-color","#A0DAA9");
+       $(".black-bg").css("background-color","#1e1e1e");
+       //$("nav").removeClass( "navbar-dark" ).addClass( "navbar-light text-dark" );
+       //$("nav").removeClass( "navbar-light text-dark" ).addClass( "navbar-dark" );
+       $("nav").toggleClass("blue-bg");
+       $("footer").toggleClass("blue-bg"); 
+       */
+   });
+
+    $("#toggle-lg").on("click", function(event){
+        
+   });
+  });
