0d5768d74a053a0c50e1d49e21cfbab6205c2d9f
leaddn <leadieudonat@gmail.com>
2021-07-10 18:40:31 +0200

    french-english version

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+ /*
 $(document).ready(function() {
- 
+
     $("#toggle-mode").on("click", function(event){
-        /*
        $("body").toggleClass("light-mode");
        $("body").toggleClass("dark-mode");
        $(".black-bg").css("background-color","#A0DAA9");
@@ -10,10 +10,40 @@ $(document).ready(function() {
        //$("nav").removeClass( "navbar-light text-dark" ).addClass( "navbar-dark" );
        $("nav").toggleClass("blue-bg");
        $("footer").toggleClass("blue-bg"); 
-       */
+       
    });
+   
+   $("button").click(function(){
+       $("#french").load("french.txt");
+    });
+});
+*/
 
-    $("#toggle-lg").on("click", function(event){
-        
-   });
-  });
+/*
+$(document).ready(function() {
+    $("#english").load("english.html");
+    $("#toggle-lg").click(function(){
+        $("#english").load("french.html");
+        //$("#english").load("english.html");
+     });
+     $("#toggle-lg").click(function(){
+        $("#english").load("english.html");
+        //$("#english").load("english.html");
+     });
+});
+*/
+$(document).ready(function() {
+    $("#english").load("english.html");
+    $("#toggle-lg").on('change', function() {
+        if ($(this).is(':checked')) {
+            $(this).attr('value', 'true');
+            $("#english").load("french.html");
+            //alert($(this).val());
+        }
+        else {
+            $(this).attr('value', 'false');
+            $("#english").load("english.html");
+            //alert($(this).val());
+        }
+    });
+});
